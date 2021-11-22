e5855d769a9b2c4f0a8e697c7dbcdd52f1bd2cef
Mark Mackey <ethereumdreamer@gmail.com>
2021-11-22 13:28:43 -0600

    Cleaned up code and added more options

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
+use chrono;
 use clap::{App, Arg};
-use colour::{cyan_ln, green_ln, red_ln};
-use hyper::body::Bytes;
 use hyper::server::conn::AddrStream;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server};
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use std::collections::HashSet;
 use std::convert::Infallible;
-use std::future::Future;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
+use termion::color;
 
 mod utils;
-use utils::SnoopError;
+use utils::{RpcErrorResponse, RpcRequest, SnoopError};
 
 struct Inner {
     dest_uri: String,
-    suppress: HashSet<String>,
+    suppress_ok: Option<HashSet<String>>,
+    suppress_all: Option<HashSet<String>>,
+    //      (cyan,   red,    green,  reset)
+    colors: (String, String, String, String),
 }
 
 #[derive(Clone)]
@@ -26,36 +26,23 @@ struct SnoopContext {
     inner: Arc<Inner>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct RpcRequest {
-    id: u8,
-    jsonrpc: String,
-    method: String,
-    params: Vec<Value>,
-}
-#[derive(Debug, Deserialize, Serialize)]
-struct RpcError {
-    code: i32,
-    message: String,
-}
-#[derive(Debug, Deserialize, Serialize)]
-struct RpcErrorResponse {
-    id: u8,
-    jsonrpc: String,
-    error: RpcError,
-}
-
 async fn copy_request(
     source_request: Request<Body>,
     context: &SnoopContext,
-) -> Result<(Request<Body>, Bytes), SnoopError> {
-    let (parts, source_body) = source_request.into_parts();
-    let source_bytes = hyper::body::to_bytes(source_body).await?;
+) -> Result<(Request<Body>, String), SnoopError> {
+    let (parts, request_body) = source_request.into_parts();
+    let request_bytes = hyper::body::to_bytes(request_body).await?;
+
+    let request_json = {
+        // Just return an error response if Utf8 conversion fails
+        let json_str = std::str::from_utf8(&request_bytes)?;
+        jsonxf::pretty_print(json_str).unwrap_or_else(|_| json_str.to_string())
+    };
 
     let mut dest_request = Request::builder()
         .method(parts.method)
         .uri(&context.inner.dest_uri)
-        .body(Body::from(source_bytes.clone()))?;
+        .body(Body::from(request_bytes))?;
 
     for (key, value) in parts.headers.iter() {
         dest_request
@@ -63,10 +50,10 @@ async fn copy_request(
             .insert(key.clone(), value.clone());
     }
 
-    Ok((dest_request, source_bytes))
+    Ok((dest_request, request_json))
 }
 
-async fn get_response(dest_request: Request<Body>) -> Result<(Response<Body>, Bytes), SnoopError> {
+async fn get_response(dest_request: Request<Body>) -> Result<(Response<Body>, String), SnoopError> {
     let dest_client = Client::new();
     let response = dest_client.request(dest_request).await?;
 
@@ -74,63 +61,124 @@ async fn get_response(dest_request: Request<Body>) -> Result<(Response<Body>, By
     let response_version = response.version();
     let response_bytes = hyper::body::to_bytes(response.into_body()).await?;
 
+    let response_json = {
+        // Just return an error response if Utf8 conversion fails
+        let json_str = std::str::from_utf8(&response_bytes)?;
+        jsonxf::pretty_print(json_str).unwrap_or_else(|_| json_str.to_string())
+    };
+
     let source_response = Response::builder()
         .status(response_status)
         .version(response_version)
-        .body(Body::from(response_bytes.clone()))?;
+        .body(Body::from(response_bytes))?;
 
-    Ok((source_response, response_bytes))
+    Ok((source_response, response_json))
+}
+
+fn print_request_response(request_json: String, response_json: String, context: &SnoopContext) {
+    let (cyan, red, green, reset) = &context.inner.colors;
+    let now = chrono::offset::Local::now()
+        .format("%b %e %T%.3f %Y")
+        .to_string();
+    match (
+        serde_json::from_str::<RpcRequest>(&request_json),
+        serde_json::from_str::<RpcErrorResponse>(&response_json),
+    ) {
+        (Ok(rpc_request), Ok(_)) => {
+            if !context
+                .inner
+                .suppress_all
+                .as_ref()
+                .map(|all| all.contains(&rpc_request.method.to_string()))
+                .unwrap_or(false)
+            {
+                println!("{} REQUEST\n{}{}{}", now, cyan, request_json, reset);
+                println!("{} RESPONSE\n{}{}{}", now, red, response_json, reset);
+            }
+        }
+        (Ok(rpc_request), Err(_)) => {
+            if !context
+                .inner
+                .suppress_all
+                .as_ref()
+                .map(|all| all.contains(&rpc_request.method.to_string()))
+                .unwrap_or(false)
+                && !context
+                    .inner
+                    .suppress_ok
+                    .as_ref()
+                    .map(|ok| ok.contains(&rpc_request.method.to_string()))
+                    .unwrap_or(false)
+            {
+                println!("{} REQUEST\n{}{}{}", now, cyan, request_json, reset);
+                println!("{} RESPONSE\n{}{}{}", now, green, response_json, reset);
+            }
+        }
+        (Err(_), err_res) => {
+            println!(
+                "{} WARNING: request not formatted as JSON-RPC request:\n{}",
+                now, request_json,
+            );
+            let color = match err_res {
+                Ok(_) => red,
+                Err(_) => green,
+            };
+            println!("{} RESPONSE\n{}{}{}", now, color, response_json, reset);
+        }
+    }
 }
 
 async fn handle_request(
     context: SnoopContext,
     _addr: SocketAddr,
     source_request: Request<Body>,
-) -> Result<Response<Body>, SnoopError> {
-    let (dest_request, source_bytes) = copy_request(source_request, &context).await?;
+) -> Result<Response<Body>, Infallible> {
+    let (dest_request, request_json) = match copy_request(source_request, &context).await {
+        Ok(result) => result,
+        Err(e) => {
+            let error_body = {
+                let rpc_error = RpcErrorResponse::from(("Error processing request", e));
+                serde_json::to_string_pretty(&rpc_error)
+                    .unwrap_or_else(|_| serde_json::json!(rpc_error).to_string())
+            };
+            let (_, red, _, reset) = &context.inner.colors;
+            println!("{}{}{}", red, error_body, reset);
+            let source_response = Response::builder()
+                .status(500)
+                .body(Body::from(error_body))
+                .unwrap();
 
-    // Just return an error response if Utf8 conversion fails
-    let source_json = std::str::from_utf8(&source_bytes)?;
-
-    let (source_response, response_bytes) = get_response(dest_request).await?;
-
-    let response_json = match serde_json::from_str::<RpcRequest>(source_json) {
-        Ok(rpc_request)
-            if !context
-                .inner
-                .suppress
-                .contains(&rpc_request.method.to_string()) =>
-        {
-            cyan_ln!("{}", jsonxf::pretty_print(source_json)?);
-            Some(std::str::from_utf8(&response_bytes)?)
+            return Ok(source_response);
         }
-        Err(_) => {
-            println!(
-                "WARNING: request not formatted as RPC request:\n{}",
-                source_json
-            );
-            Some(std::str::from_utf8(&response_bytes)?)
-        }
-        _ => None,
     };
 
-    if let Some(response_json) = response_json {
-        if let Ok(_rpc_error) = serde_json::from_str::<RpcErrorResponse>(response_json) {
-            red_ln!("{}", jsonxf::pretty_print(response_json)?);
-        } else {
-            green_ln!("{}", jsonxf::pretty_print(response_json)?);
+    let (source_response, response_json) = match get_response(dest_request).await {
+        Ok(result) => result,
+        Err(e) => {
+            let error_body = {
+                let rpc_error = RpcErrorResponse::from(("Error processing response", e));
+                serde_json::to_string_pretty(&rpc_error)
+                    .unwrap_or_else(|_| serde_json::json!(rpc_error).to_string())
+            };
+            let source_response = Response::builder()
+                .status(500)
+                .body(Body::from(error_body.clone()))
+                .unwrap();
+            (source_response, error_body)
         }
-    }
+    };
+
+    print_request_response(request_json, response_json, &context);
 
     Ok(source_response)
 }
 
 #[tokio::main]
 async fn main() {
-    let matches = App::new("Json Snooping Tool")
+    let matches = App::new("JSON-RPC Snooping Tool")
         .version("0.1")
         .author("Mark Mackey <ethereumdreamer@gmail.com>")
-        .about("Proxies an eth1 http json endpoint and dumps requests and responses to screen")
+        .about("Proxies an http JSON-RPC endpoint and dumps requests and responses to screen")
         .arg(
             Arg::with_name("bind-address")
                 .short("b")
@@ -150,32 +198,58 @@ async fn main() {
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("suppress")
+            Arg::with_name("no-color")
+                .long("no-color")
+                .short("n")
+                .required(false)
+                .takes_value(false)
+                .help("Do not use terminal colors in output"),
+        )
+        .arg(
+            Arg::with_name("suppress-ok")
                 .short("s")
-                .long("suppress")
-                .help("methods to suppress")
+                .long("suppress-ok")
+                .help("Suppress successful JSON RPC method calls of this type in output (can specify more than one)")
                 .multiple(true)
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("ETH1_ENDPOINT")
-                .help("Eth1 endpoint to forward incoming requests to")
+            Arg::with_name("suppress-all")
+                .short("S")
+                .long("suppress-all")
+                .help("Suppress success or error JSON RPC method calls of this type in output (can specify more than one)")
+                .multiple(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("RPC_ENDPOINT")
+                .help("JSON-RPC endpoint to forward incoming requests to")
                 .required(true)
                 .index(1),
         )
         .get_matches();
 
-    let mut suppress = HashSet::new();
-    if let Some(values) = matches.values_of("suppress") {
-        for value in values {
-            suppress.insert(value.to_string());
-        }
-    }
+    let color_tuple = if matches.is_present("no-color") {
+        (String::new(), String::new(), String::new(), String::new())
+    } else {
+        (
+            color::Fg(color::Cyan).to_string(),
+            color::Fg(color::Red).to_string(),
+            color::Fg(color::Green).to_string(),
+            color::Fg(color::Reset).to_string(),
+        )
+    };
 
     let context = SnoopContext {
         inner: Arc::new(Inner {
-            dest_uri: matches.value_of("ETH1_ENDPOINT").unwrap().to_string(),
-            suppress,
+            dest_uri: matches.value_of("RPC_ENDPOINT").unwrap().to_string(),
+            colors: color_tuple,
+            suppress_ok: matches
+                .values_of("suppress-ok")
+                .map(|values| values.into_iter().map(|s| s.to_string()).collect()),
+            suppress_all: matches
+                .values_of("suppress-all")
+                .map(|values| values.into_iter().map(|s| s.to_string()).collect()),
         }),
     };
 
@@ -190,50 +264,27 @@ async fn main() {
         let addr = conn.remote_addr();
 
         // Create a `Service` for responding to the request.
-        let service = service_fn(move |req| log_error(handle_request(context.clone(), addr, req)));
+        let service = service_fn(move |req| handle_request(context.clone(), addr, req));
 
         // Return the service to hyper.
         async move { Ok::<_, Infallible>(service) }
     });
 
-    // Run the server like above...
     match SocketAddr::from_str(&format!(
         "{}:{}",
         matches.value_of("bind-address").unwrap(),
         matches.value_of("port").unwrap()
     )) {
-        Ok(socket) => {
-            let server = Server::bind(&socket).serve(make_service);
-            if let Err(e) = server.await {
-                eprintln!("server error: {}", e);
-            }
-        }
-        Err(e) => eprintln!("Error parsing listen address: {:?}", e),
-    }
-}
-
-async fn log_error<F>(handle_func: F) -> Result<Response<Body>, Infallible>
-where
-    F: Future<Output = Result<Response<Body>, SnoopError>>,
-{
-    match handle_func.await {
-        Ok(response) => Ok(response),
-        Err(snoop_error) => {
-            let error_body = serde_json::json!(RpcErrorResponse {
-                id: 1,
-                jsonrpc: "2.0".to_string(),
-                error: RpcError {
-                    code: -32603,
-                    message: format!("{:?}", snoop_error)
+        Ok(socket) => match Server::try_bind(&socket) {
+            Ok(server) => {
+                if let Err(e) = server.serve(make_service).await {
+                    eprintln!("server error: {}", e);
                 }
-            })
-            .to_string();
-            red_ln!("{}", error_body);
-            let source_response = Response::builder()
-                .status(500)
-                .body(Body::from(error_body))
-                .unwrap();
-            Ok(source_response)
-        }
+            }
+            Err(e) => {
+                eprintln!("Unable to bind to socket: {}", e);
+            }
+        },
+        Err(e) => eprintln!("Error parsing listen address: {:?}", e),
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use hyper::http::Error as HyperHttpError;
 use hyper::Error as HyperError;
-use serde_json::Error as SerdeError;
+use serde::{Deserialize, Serialize};
 use std::str::Utf8Error;
 
 #[derive(Debug)]
@@ -8,8 +8,6 @@ pub enum SnoopError {
     HyperError(HyperError),
     HyperHttpError(HyperHttpError),
     StringConversion(Utf8Error),
-    SerdeError(SerdeError),
-    Json(String),
 }
 
 impl From<HyperHttpError> for SnoopError {
@@ -30,14 +28,45 @@ impl From<Utf8Error> for SnoopError {
     }
 }
 
-impl From<SerdeError> for SnoopError {
-    fn from(e: SerdeError) -> Self {
-        SnoopError::SerdeError(e)
-    }
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RpcRequest {
+    pub id: u8,
+    pub jsonrpc: String,
+    pub method: String,
+    pub params: Vec<serde_json::Value>,
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RpcError {
+    pub code: i32,
+    pub message: String,
+}
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RpcErrorResponse {
+    pub id: u8,
+    pub jsonrpc: String,
+    pub error: RpcError,
 }
 
-impl From<String> for SnoopError {
-    fn from(err_str: String) -> Self {
-        SnoopError::Json(err_str)
+impl From<(&str, SnoopError)> for RpcErrorResponse {
+    fn from(pair: (&str, SnoopError)) -> Self {
+        let (prefix, snoop_error) = pair;
+        Self {
+            id: 1,
+            jsonrpc: "2.0".to_string(),
+            error: RpcError {
+                code: -32603,
+                message: match snoop_error {
+                    SnoopError::HyperError(e) => {
+                        format!("{}: encountered hyper error: {:?}", prefix, e)
+                    }
+                    SnoopError::HyperHttpError(e) => {
+                        format!("{}: encountered hyper::http error: {:?}", prefix, e)
+                    }
+                    SnoopError::StringConversion(e) => {
+                        format!("{}: error converting to Utf-8: {:?}", prefix, e)
+                    }
+                },
+            },
+        }
     }
 }
